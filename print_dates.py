a7b4963b14cbebd02ea8b7b8310d850bd1b92548
Georgi Knox <georgiknox@gmail.com>
2015-10-02 11:08:22 -0400

    list comprehensions yo

--- print_dates.py
+++ print_dates.py
@@ -4,12 +4,10 @@ def daterange(start_date, end_date):
     for n in range(int ((end_date - start_date).days)):
         yield start_date + timedelta(n)
 
-# helpful to construct curl for elasticsearch index deletion
+# helpful to construct curl for elasticsearch index deletion, search etc.
 if __name__ == '__main__':
     start_date = date(2015, 7, 27)
     end_date = date(2015, 9, 18)
-    dates = []
-    for single_date in daterange(start_date, end_date):
-        dates.append(single_date.strftime("%Y_%m_%d"))
+    dates = [d.strftime("%Y_%m_%d") for d in daterange(start_date, end_date)]
 
     print ",".join(dates)
