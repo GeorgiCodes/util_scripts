8378dc4d1fa188f44effb5cecedeec54e7a0c505
Georgi Knox <georgiknox@gmail.com>
2015-10-02 13:18:37 -0400

    even simpler

--- print_dates.py
+++ print_dates.py
@@ -1,13 +1,9 @@
 from datetime import timedelta, date
 
-def daterange(start_date, end_date):
-    for n in range(int ((end_date - start_date).days)):
-        yield start_date + timedelta(n)
-
 # helpful to construct curl for elasticsearch index deletion, search etc.
 if __name__ == '__main__':
     start_date = date(2015, 7, 27)
     end_date = date(2015, 9, 18)
-    dates = [d.strftime("%Y_%m_%d") for d in daterange(start_date, end_date)]
+    dates = [(start_date + timedelta(days=d)).strftime("%Y_%m_%d") for d in range(0, (end_date-start_date).days + 1)]
 
     print ",".join(dates)
